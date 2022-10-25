287f7617372b144e3f9d1f65084d5fb6739b5268
29-GlennTho <114901634+27-GlennTho@users.noreply.github.com>
2022-10-25 10:56:14 +0800

    Jump added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
-
+    public GameObject player;
+    public Rigidbody rb;
+    private float jumpForce=3.0f;
+    public Vector3 jump;
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 3.0f, 0.0f);
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
     }
 
     void Update()
     {
+        if(player.y>=100)
+        {
+
+        }
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+        }
     }
 }
